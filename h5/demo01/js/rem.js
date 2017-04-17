e5e46ca809ee9941cae55d87e5a8f296d34a83ba
dali <542621868@qq.com>
2017-04-17 15:19:56 +0800

    'demo01'

---
+++ h5/demo01/js/rem.js
@@ -0,0 +1,12 @@
+!(function(doc, win) {
+    var docEle = doc.documentElement,
+        evt = "onorientationchange" in window ? "orientationchange" : "resize",
+        fn = function() {
+            var width = docEle.clientWidth > 750? 750:docEle.clientWidth;
+            width && (docEle.style.fontSize = 20 * (width / 375) + "px");
+        };
+
+    win.addEventListener(evt, fn, false);
+    doc.addEventListener("DOMContentLoaded", fn, false);
+
+}(document, window));
